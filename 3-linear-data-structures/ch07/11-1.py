61b70a91f36392829303ca1c279e2972b1fcb71e
ilotoki0804 <ilotoki0804@gmail.com>
2024-01-22 19:32:47 +0900

    refactoring 11-1

--- 3-linear-data-structures/ch07/11-1.py
+++ 3-linear-data-structures/ch07/11-1.py
@@ -3,15 +3,16 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        out = []
-        p = 1
-        # 왼쪽 곱셈
-        for i in range(0, len(nums)):
-            out.append(p)
-            p = p * nums[i]
-        p = 1
-        # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
-        for i in range(len(nums) - 1, 0 - 1, -1):
-            out[i] = out[i] * p
-            p = p * nums[i]
-        return out
+        result = []
+
+        left_accumulated = 1
+        for number in nums:
+            result.append(left_accumulated)
+            left_accumulated *= number
+
+        right_accumulated = 1
+        for i in range(0, len(nums))[::-1]:
+            result[i] *= right_accumulated
+            right_accumulated *= nums[i]
+
+        return result
