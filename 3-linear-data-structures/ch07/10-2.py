b933b868d185ed471590d6ce155b3aff3fea1d32
ilotoki0804 <ilotoki0804@gmail.com>
2024-01-22 19:36:03 +0900

    Use comprehension

--- 3-linear-data-structures/ch07/10-2.py
+++ 3-linear-data-structures/ch07/10-2.py
@@ -3,12 +3,10 @@ from typing import List
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        sum = 0
         nums.sort()
 
-        for i, n in enumerate(nums):
-            # 짝수 번째 값의 합 계산
-            if i % 2 == 0:
-                sum += n
-
-        return sum
+        return sum(
+            number
+            for i, number in enumerate(nums)
+            if i % 2 == 0
+        )
